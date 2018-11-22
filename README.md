6681f29974ac7a1b4182f8dc54befde1d1c926a7
Gururaj Dharani <dharani.gururaj@gmail.com>
2018-11-22 23:39:42 +0530

    add: compression script and readme

---
+++ README.md
@@ -0,0 +1,19 @@
+### Image compression Algo mozjpeg
+
+Install locally
+
+```
+npm install
+```
+
+create source folder and add all images to the folder
+
+```
+mkdir src
+```
+
+Run script
+
+```
+node compress.js
+```
---
+++ compress.js
@@ -0,0 +1,72 @@
+var compress_images = require('compress-images');
+
+const INPUT_path_to_your_images = 'src/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+const OUTPUT_path = 'build/';
+
+compress_images(INPUT_path_to_your_images, OUTPUT_path, {
+  compress_force: false,
+  statistic: true,
+  autoupdate: true,
+  pathLog: './log/lib/compress-images'
+}, false, {
+  jpg: {
+    engine: 'jpegRecompress',
+    command: ['--quality', 'high', '--min', '60']
+  }
+}, {
+  png: {
+    engine: 'pngquant',
+    command: ['--quality=20-50']
+  }
+}, {
+  svg: {
+    engine: 'svgo',
+    command: '--multipass'
+  }
+}, {
+  gif: {
+    engine: 'gifsicle',
+    command: ['--colors', '64', '--use-col=web']
+  }
+}, function(err, completed) {
+  if (err !== null) {
+    //---------------------------------------
+    //if you get an ERROR from 'jpegRecompress' ---> We can use alternate config of compression
+    //---------------------------------------
+    if (err.engine === 'jpegRecompress') {
+      compress_images(err.input, err.output, {
+        compress_force: false,
+        statistic: true,
+        autoupdate: true
+      }, false, {
+        jpg: {
+          engine: 'mozjpeg',
+          command: ['-quality', '60']
+        }
+      }, {
+        png: {
+          engine: false,
+          command: false
+        }
+      }, {
+        svg: {
+          engine: false,
+          command: false
+        }
+      }, {
+        gif: {
+          engine: false,
+          command: false
+        }
+      }, function(err) {
+        if (err !== null) {
+          //Alternative config of compression
+
+        }
+      });
+    }
+    //---------------------------------------
+
+  }
+
+});
